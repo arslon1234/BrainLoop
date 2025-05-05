997343b33a98f9cba340006e52dbd7b955037210
Arslonbek <roziboyevarslon0110@gmail.com>
2025-05-05 16:19:52 +0500

    add tooltip for format code

--- src/widgets/playground/code-editor/header.tsx
+++ src/widgets/playground/code-editor/header.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
-import { Dropdown, MenuProps } from 'antd';
+import { Tooltip, Dropdown, MenuProps } from 'antd';
 import { DownOutlined } from '@ant-design/icons';
+import { CgFormatLeft } from "react-icons/cg";
 import './header.css';
 
 const CodeEditorHeader = () => {
@@ -11,6 +12,12 @@ const CodeEditorHeader = () => {
     { key: 'Python', label: 'Python' },
     { key: 'Java', label: 'Java' },
     { key: 'C++', label: 'C++' },
+    { key: 'TypeScript', label: 'TypeScript' },
+    { key: 'Ruby', label: 'Ruby' },
+    { key: 'Go', label: 'Go' },
+    { key: 'Rust', label: 'Rust' },
+    { key: 'PHP', label: 'PHP' },
+    { key: 'C#', label: 'C#' },
   ];
 
   const handleMenuClick: MenuProps['onClick'] = ({ key }) => {
@@ -19,6 +26,7 @@ const CodeEditorHeader = () => {
 
   return (
     <div className="p-2 bg-transparent w-full rounded-tl-lg rounded-tr-lg flex justify-between items-center border-b-[0.9px] border-[#333]">
+      <div>
       <Dropdown
         menu={{ items, onClick: handleMenuClick }}
         trigger={['click']}
@@ -28,6 +36,14 @@ const CodeEditorHeader = () => {
           {selectedLanguage} <DownOutlined className='text-[12px]'/>
         </button>
       </Dropdown>
+      </div>
+      <div>
+        <Tooltip title="Format Code" placement='top'>
+        <button className='cursor-pointer hover:bg-[#333] p-1 rounded-[4px]'>
+        <CgFormatLeft className='text-[17px] text-[#999]'/>
+        </button>
+        </Tooltip>
+      </div>
     </div>
   );
 };
